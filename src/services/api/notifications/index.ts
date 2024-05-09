bdb4551cfc46a08aa68b4cfbcf149bf9135b9015
luis <l.ramirez@intrare.mx>
2024-05-09 12:27:18 -0600

    Handle errors

--- src/services/api/notifications/index.ts
+++ src/services/api/notifications/index.ts
@@ -6,45 +6,67 @@ type NewNotification = {
 }
 
 export const newNotification = async(data : NewNotification) => {
-    const res = await fetch(`${url}/v1/notifications/notification`,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(data)
-    });
-    const resData = await res.json();
-    return {
-        data: resData.data,
-        success: resData.success
+    try {
+        const res = await fetch(`${url}/v1/notifications/notification`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(data)
+        });
+        const resData = await res.json();
+        return {
+            data: resData.data,
+            success: resData.success
+        }
+    } catch (error) {
+        return {
+            data: {},
+            success: false
+        }
     }
+
 }
 
 export const getNotifications = async(id: string) => {
-    const res = await fetch(`${url}/v1/notifications/notifications?id=${id}`,{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-          },
-    });
-    const resData = await res.json();
-    return {
-        data: resData.data,
-        success: resData.success
+    try {
+        const res = await fetch(`${url}/v1/notifications/notifications?id=${id}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+        });
+        const resData = await res.json();
+        return {
+            data: resData.data,
+            success: resData.success
+        }   
+    } catch (error) {
+        return {
+            data: {},
+            success: false
+        }
     }
 }
 
 export const readNotification = async(id: string) => {
-    const res = await fetch(`${url}/v1/notifications/notification`,{
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify({id})
-    });
-    const resData = await res.json();
-    return {
-        data: resData.data,
-        success: resData.success
+    try {
+        const res = await fetch(`${url}/v1/notifications/notification`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({id})
+        });
+        const resData = await res.json();
+        return {
+            data: resData.data,
+            success: resData.success
+        }
+    } catch (error) {
+        return {
+            data: {},
+            success: false
+        }
     }
 }
